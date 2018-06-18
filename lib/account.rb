05537b398860d5d8a53e809b56c02e079201c9b8
benfurber <ben@furber-consultancy.com>
2018-06-18 14:09:44 +0100

    Add basic Account methods

--- lib/account.rb
+++ lib/account.rb
@@ -1,7 +1,17 @@
+# Main Account class a user sets up
 class Account
-  attr_reader :balance
+  attr_reader :balance, :log
 
   def initialize
     @balance = 0
+    @log = []
+  end
+
+  def deposit
+    @log.push('deposit')
+  end
+
+  def withdraw
+    @log.push('withdraw')
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,5 +7,29 @@ describe Account do
     it 'balance is zero' do
       expect(subject.balance).to eq 0
     end
+
+    it 'log is empty' do
+      expect(subject.log).to be_empty
+    end
+  end
+
+  context '#deposit' do
+    it 'adds an item to the log' do
+      expect {
+        subject.deposit
+      }.to change {
+        subject.log.length
+      }.by 1
+    end
+  end
+
+  context '#withdraw' do
+    it 'adds an item to the log' do
+      expect {
+        subject.withdraw
+      }.to change {
+        subject.log.length
+      }.by 1
+    end
   end
 end
