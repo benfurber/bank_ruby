cf3cdd2c539d1c1a0bb07026f609a67618fd3060
benfurber <ben@furber-consultancy.com>
2018-06-19 15:00:47 +0100

    Change all #legal_ to #valid_? to make intent clearer

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -4,35 +4,35 @@ class Transaction
 
   TRANSACTION_TYPES = ['deposit', 'withdraw']
 
-  def initialize(transaction_type, amount, date = nil)
-    @transaction_type = legal_transaction(transaction_type)
-    @amount = legal_amount(amount)
-    @date = legal_date(date)
+  def initialize(transaction_type, amount, date = add_date_now)
+    @transaction_type = transaction_type if valid_transaction?(transaction_type)
+    @amount = amount if valid_amount?(amount)
+    @date = date if valid_date?(date)
   end
 
   private
 
-  def legal_transaction(transaction_type)
+  def add_date_now
+    date = Time.now
+    return date.strftime("%d/%m/%Y")
+  end
+
+  def valid_transaction?(transaction_type)
     unless TRANSACTION_TYPES.include?(transaction_type)
       raise 'Not a legal transaction type'
     end
 
-    transaction_type
+    true
   end
 
-  def legal_amount(amount)
+  def valid_amount?(amount)
     raise 'Not a number' unless amount.is_a? Numeric
     raise 'Not a positive number' unless amount > 0
 
-    amount
+    true
   end
 
-  def legal_date(date)
-    if date.nil?
-      date = Time.now
-      return date.strftime("%d/%m/%Y")
-    end
-
+  def valid_date?(date)
     unless date =~ /[0-9]{2}\/[0-9]{2}\/[0-9]{4}/
       # Yes, this only checks that the string is two numbers, slash, two
       # numbers, slash four numbers. I could have used the full (leap year
@@ -41,6 +41,6 @@ class Transaction
       raise 'Date must be in dd/mm/yyyy format as a string'
     end
 
-    date
+    true
   end
 end
